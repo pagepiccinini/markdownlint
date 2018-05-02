1924865a4f45dcb29091e170e61d93ec79e80dc1
Page Piccinini <ppiccinini@classy.org>
2018-05-01 17:19:46 -0700

    [DRPT-561] ENH: Add rules.

---
+++ lib/mdl/styles/data_insights.rb
@@ -0,0 +1,10 @@
+all
+rule 'MD004', :style => :asterisk  # Unordered list style
+rule 'MD007', :indent => 4         # Unordered list indentation
+rule 'MD029', :style => :ordered   # Ordered list item prefix
+
+exclude_rule 'MD012'  # Multiple consecutive blank lines <-- need to update for just between main headers
+exclude_rule 'MD013'  # Line length
+exclude_rule 'MD025'  # Multiple top level headers in the same document
+exclude_rule 'MD026'  # Trailing punctuation in header
+exclude_rule 'MD041'  # First line in file should be a top level header
